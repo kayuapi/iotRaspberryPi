f649f275344aef90605671f6be492ed3084d2f79
cong <zhicong.tay@gmail.com>
2019-11-11 17:18:37 +0800

    url update

--- boxClient.py
+++ boxClient.py
@@ -7,7 +7,7 @@ class boxClient:
 	def __init__(self):
 		API_KEY = '548451321avav'
 		authorizationValue = 'Bearer ' + API_KEY;
-		self.url = 'http://localhost:8000/api/compartments/'
+		self.url = 'https://chmbox.tech/api/compartments/'
 		self.headers = {'content-type': 'application/json', 'Accept': 'application/json', 'Authorization': authorizationValue}
 		
 	def changePasscode(self, compartmentNumber, passcode, act):
